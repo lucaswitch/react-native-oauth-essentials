bcc55140c2c02ed5339bb516997f9f3f224460b2
Lucas Alves <lucas.developing@gmail.com>
2026-01-23 01:23:15 -0300

    feat(docs): added docs branch

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,14 +19,8 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
   url: 'https://lucaswitch.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/react-native-oauth-essentials',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lucaswitch', // Usually your GitHub org/user name.
   projectName: 'react-native-oauth-essentials', // Usually your repo name.
   trailingSlash: false,
