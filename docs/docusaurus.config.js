cf4300ada01b770516425b2fe4fdd4647d9db0a2
Lucas Alves <lucas.developing@gmail.com>
2026-01-24 13:54:26 -0300

    feat(docs): add github docs ci

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -27,9 +27,6 @@ const config = {
   projectName: 'react-native-oauth-essentials', // Repo name
   deploymentBranch: 'gh-pages', // optional, default is 'gh-pages'
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
